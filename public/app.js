e0e8a299403e878c8a1325e6cb0d5b1e5228b3ad
kabirsethionw <kabirsethione@gmail.com>
2021-05-13 17:46:16 +0530

    Added form, and data posting functionality.

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@
   var firebaseConfig = {
     apiKey: "AIzaSyA4Y30-d15eYZ2n03tGKvF_4o-nVwq4cK8",
     authDomain: "lets-gtok-internship.firebaseapp.com",
+    databaseURL: "https://lets-gtok-internship-default-rtdb.firebaseio.com",
     projectId: "lets-gtok-internship",
     storageBucket: "lets-gtok-internship.appspot.com",
     messagingSenderId: "973498021574",
@@ -17,18 +18,39 @@
 const dbRef = firebase.database().ref();
 const usersRef = dbRef.child('users');
 
+//GET
+var ptr = 0;
+var usrPosts = ["post1", "post2", "post1", "post2"];
+console.log(document.getElementById(usrPosts[0]).children[1].innerHTML)
 usersRef.on("child_added", snap => {
-    alert(snap.val().name);
-    console.log(snap.Key);
+  if(ptr>3) return;
+  document.getElementById(usrPosts[ptr]).children[1].innerHTML = snap.val().message;
+  let usrname = document.getElementById(usrPosts[ptr]).children[2].getElementsByTagName("strong")[0];
+  usrname.innerHTML = "@" + snap.val().name.replace(/\s/g,"_");
+  let email = document.createTextNode(snap.val().email)
+  document.getElementById(usrPosts[ptr]).children[2].appendChild(email);
+  
+  console.log(snap.val());
+  console.log("Hello ",document.getElementById(usrPosts[ptr]).children[1].innerHTML)
+  ptr++;
 });
-// alert(usersRef);
-// console.log(usersRef)
-// var usrPosts = ["post1", "post2", "post3", "post4"];
-// var ptr = 0;
-// for(let post of usrPosts){
-//     document.getElementById(post).children[1].innerHTML = usersRef[ptr];
-//     ptr++;
-// }
 
+//POST
+document.getElementById("post-message").addEventListener("click", function(){
+  var data = {}
+  for(let i = 1; i < 8; i += 3){
+    let key = document.getElementById("contact-form").children[i].getAttribute("name");
+    let value = document.getElementById("contact-form").children[i].value;
+    data[key] = value;
+  }
+  console.log(data)
 
+  usersRef.push(data, function(){
+    console.log("Saving data...");
+  }).then(res => {
+    console.log("address key = " + res.key) ;
+    alert("Data saved U+2713")
+  });
+  
+});
 
